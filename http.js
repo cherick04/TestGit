08a3d733d7e607f24709b541ba8cf4b777014b49
dmhjin <mh9298@gmail.com>
2016-03-08 21:50:26 -0500

    Update http.js

--- http.js
+++ http.js
@@ -1,23 +1,32 @@
+
 // Include http module.
-//var http = require("http");
+var http = require("http");
 var fs = require('fs');
 var formidable = require("formidable");
 var util = require('util');
+// Create the server. Function passed as parameter is called on every request made.
+// request variable holds all request parameters
+// response variable allows you to do anything with response sent to the client.
+
 var express    = require('express');
+var bodyParser = require('body-parser');
 var mysql      = require('mysql');
-var app = express();
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-app.get('/', function(req , res){
-	displayForm(res);
+var server  = http.createServer(function (req, res) {
+	if (req.method.toLowerCase() == 'get'){
+		displayForm(res);
+	} else if (req.method.toLowerCase() == 'post'){
+		processForm(req, res);
+	}
+	
 });
 
-app.post('/', function (req , res){
-	processForm(req, res);
-})
-
 function displayForm(res) {
-    fs.readFile('Registration_Form.html', function (err, data) {
+    fs.readFile('Registration.Form.html', function (err, data) {
         res.writeHead(200, {
             'Content-Type': 'text/html',
                 'Content-Length': data.length
@@ -29,7 +38,8 @@ function displayForm(res) {
 
 
 function processForm(req, res) {
-	var form = new formidable.IncomingForm();
+	
+    var form = new formidable.IncomingForm();
 
     form.parse(req, function (err, fields, files) {
         //Store the data from the fields in your data store.
@@ -37,34 +47,37 @@ function processForm(req, res) {
         //on your application.
 	var connection = mysql.createConnection({
 	  host     : 'localhost',
-	  user     : 'simon',
-	  password : 'pass',
+	  user     : 'admin',
+	  password : 'admin',
 	  database : 'users'
 	});
 
-
+	var app = express();
+	app.use(express.static('public'));
 	connection.connect(function(err){
 		if(!err){
-			console.log("Database is connected ...nn");}
+			console.log("Database is connected ...nn");
+			console.log("I am here");}
 		else{
 			console.log("Error connection database ...nn");}
 	});
-
-
-
-	var query = connection.query('INSERT INTO userinfo set ?', fields , function(err, rows, fields){
-
-	if(!err)
-		console.log('The solution is: ', rows);
-	else
-		console.log('Error while performing query');
-	connection.end();
+		
+	console.log('error here1');	
+	//app.post('/dafaq', urlencodedParser, function(req, res){
+		console.log('error here2');	
+		var query = connection.query('INSERT INTO Persons SET ?', fields, function(err, rows, fields){
+		connection.end();
+		if(!err)
+			console.log('The solution is: ', rows);
+		else
+			console.log('Error while performing query');
 		});
-
+	//});
+	//app.listen(3000);
+		
     res.writeHead(200, {
         'content-type': 'text/plain'
     });
-
     res.write('received the data:\n\n');
       res.end(util.inspect({
         field: fields,
@@ -74,5 +87,7 @@ function processForm(req, res) {
 }
 
 
-app.listen(3000, '127.0.0.1');
-console.log('Server listening on 3000 ...');
+server.listen(8080);
+//server.listen(8080, '127.0.0.1');
+console.log('Server listening on 8080 ...');
+
